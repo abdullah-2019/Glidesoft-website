167306f5f551067c8ae583ef387baa8aa3ab8a0f
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-04-08 08:33:32 +0430

    Update header.php
    all links linked in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,10 +11,10 @@
         <nav id="navmenu" class="navmenu">
             <ul>
                 <li><a href="{{url('/')}}" class="active">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#features">Features</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#pricing">Pricing</a></li>
+                <li><a href="{{url('/')}}#about">About</a></li>
+                <li><a href="{{url('/')}}#features">Features</a></li>
+                <li><a href="{{url('/')}}#services">Services</a></li>
+                <li><a href="{{url('/')}}#pricing">Pricing</a></li>
                 <!-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
         <ul>
           <li><a href="#">Dropdown 1</a></li>
@@ -37,7 +37,7 @@
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted" href="index.html#contact">Contact Now</a>
+        <a class="btn-getstarted" href="{{url('/')}}#contact">Contact Now</a>
 
     </div>
 </header>
