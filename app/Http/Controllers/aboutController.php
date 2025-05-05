dc97b2779748f2a39766a9a5e0c512b227a6fe1b
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-05-05 16:12:03 +0430

    Update aboutController.php

--- app/Http/Controllers/aboutController.php
+++ app/Http/Controllers/aboutController.php
@@ -7,61 +7,6 @@ use App\Models\Services;
 
 class ServicesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $slug)
-    {
-        // $services = Services::where('slug', $slug)->firstOrFail();
-        // return view('services.view', compact('services'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 
 }
