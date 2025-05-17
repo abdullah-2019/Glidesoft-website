bab147a4c8816025cde2346b4554510ab8601b21
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-05-17 12:29:59 +0430

    Update ExportController.php

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -20,12 +20,5 @@ class ExportController extends Controller
         ]);
     }
 
-    public function collection1()
-    {
-        return new Collection([
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]);
-    }
+    
 }
