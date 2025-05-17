a43abbe09c9f3b9cf2d9236a221a44eb30cddb47
Abdullah Hussaini <h.abdullah1394@gmail.com>
2025-05-17 11:37:32 +0430

    Update ExportController.php

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -9,5 +9,14 @@ class ExportController extends Controller
     function __construct()
     {
         $this->middleware('auth');
+
+    }
+
+    public function collection()
+    {
+        return new Collection([
+            [1, 2, 3],
+            [4, 5, 6]
+        ]);
     }
 }
